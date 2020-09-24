b292792dffadd270edb8b3bd28b16d67a7f65d00
The-Kevin <kevin.almeida1111.ka@gmail.com>
2020-09-23 22:44:10 -0300

    correção de bug na injeção de dependencia *codigo sem refatoração*

--- src/app.ts
+++ src/app.ts
@@ -13,29 +13,24 @@ import routes  from './routes';
 
     
     constructor(){
-        //this.expressServe();
         this.rotas();
-        this.socketServe();
-        this.socket();
         this.servicoHttp();
+        this.socket();
     }
 
     public servicoHttp(){
         let http = createServer(this.app)
+        this.io = SocketIO(http)
         http.listen(this.port, () => {
             console.log(`serviço http na porta ${this.port}`);
         })
     }
 
-    public rotas(){
+    public rotas(): void{
         this.app = express();
         this.app.use(routes);
     }
 
-    private socketServe(): void {
-        this.server = createServer(this.app);
-        this.io = SocketIO(this.server)
-    }
 
     public socket(){
         try{
@@ -49,17 +44,5 @@ import routes  from './routes';
             console.log(error)
         }
     }
-
-   /* public expressServe(): void {
-            //inicialização do express
-        try{
-            this.app = express()
-            this.app.listen(8080)
-            console.log(`servidor conectado na porta ${this.port}`)
-        }catch(error){
-            console.log('Não foi possivel conectar ao servidor Express', error)
-        }
-    }*/
-
 }
 new App();
